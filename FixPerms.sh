b0dba107b4a37faf506686ee49a0ab01d81b9e48
Vincent S. Cojot <vincent@cojot.name>
2006-11-27 10:17:55 +0000

    Initial revision

--- FixPerms.sh
+++ FixPerms.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: FixPerms.sh,v 1.3 2012/01/04 18:12:45 root Exp $
+# $Id: FixPerms.sh,v 1.1 2006/11/27 10:17:55 root Exp $
 #
 CHMOD_CMD=gchmod
 CHOWN_CMD=gchown
@@ -7,8 +7,8 @@ CHOWN_CMD=gchown
 if [ "$1" = "" ]; then
 	echo "Using \".\" as argument."
 	${CHOWN_CMD} root:root -R .
-	${CHMOD_CMD} -R a+Xr,og-w,u+w .
+	${CHMOD_CMD} -R a+Xr,og-w .
 else
-	${CHOWN_CMD} root:root -R "$@"
-	${CHMOD_CMD} -R a+Xr,og-w,u+w "$@"
+	${CHOWN_CMD} root:root -R $*
+	${CHMOD_CMD} -R a+Xr,og-w $*
 fi
