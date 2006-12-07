19bc0150460090818b52b8f8180f99bc7934307b
Vincent S. Cojot <vincent@cojot.name>
2006-12-07 09:54:13 +0000

    Initial revision

--- GoUpper.sh
+++ GoUpper.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 #
-# $Id: GoUpper.sh,v 1.7 2012/01/30 15:47:39 root Exp $
+# $id$
 #
-TR_CMD=/usr/bin/tr
 
-${TR_CMD} '[:upper:]' '[:lower:]' | \
 awk '# caps - capitalize 1st letter of 1st word
 
 # initialize strings
 BEGIN {
 	upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         lower = "abcdefghijklmnopqrstuvwxyz" 
-#	FS = "_|-| " ; OFS = "_"
-	FS = "_| " ; OFS = "_"
+	FS = "_" ; OFS = "_"
 }
 
 # for each input line
@@ -22,13 +19,12 @@ BEGIN {
 
 for ( i = 1 ; i <= NF; i++)
 {
+# get first character of first word
 	FIRSTCHAR = substr($i, 1, 1)
+# get position of FIRSTCHAR in lowercase array; if 0, ignore
 	if (CHAR = index(lower, FIRSTCHAR)) 
 		$i = substr(upper, CHAR, 1) substr($i, 2)
 }
-
 # print record
-	print $0
-# print record with original
-	# print ORIG " " $0
+	print ORIG " " $0
 }'
