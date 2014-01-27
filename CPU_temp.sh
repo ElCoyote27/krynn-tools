837b6701005107ea8b69b0dc496d7c8657daff43
Vincent S. Cojot <vincent@cojot.name>
2014-01-27 21:23:16 +0000

    *** empty log message ***

--- CPU_temp.sh
+++ CPU_temp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# $Id: CPU_temp.sh,v 1.7 2014/01/27 20:14:49 root Exp $
+# $Id: CPU_temp.sh,v 1.8 2014/01/27 21:23:16 root Exp $
 CPUTMP_FILE=`/bin/mktemp -p /tmp --suffix=CPU_temp`
 
 if [ ! -f ${CPUTMP_FILE} ]; then
@@ -16,7 +16,8 @@ fi
 # IPMITOOL
 if [ -x /usr/bin/ipmitool ] ; then
 #	AMB_TEMP=`/usr/bin/ipmitool sdr list|awk -F '|' '{ if (( $1 ~ /Ambient/ ) && ( $3 ~ /ok/ )) print $2}'`
-	AMB_TEMP=`/usr/sbin/ipmi-sensors -t Temperature --ignore-not-available-sensors|awk -F '|' '{ if ( $2 ~ /Ambient/ ) print $4,$5}'|xargs`
+#	AMB_TEMP=`/usr/sbin/ipmi-sensors -t Temperature --ignore-not-available-sensors|awk -F '|' '{ if ( $2 ~ /Ambient/ ) print $4,$5}'|xargs`
+	AMB_TEMP=`/usr/sbin/ipmi-sensors -s 10|awk -F '|' '{ if ( $2 ~ /Ambient/ ) print $4,$5}'|xargs`
 	if [ "x${AMB_TEMP}" != "x" ]; then
 		echo "Ambient Temp: ${AMB_TEMP}"
 	fi
