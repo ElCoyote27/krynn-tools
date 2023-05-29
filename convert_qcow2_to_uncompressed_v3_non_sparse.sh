8e81bcaa95278fc31a57d404076912588c431e0d
Vincent S. Cojot <vincent@cojot.name>
2023-05-29 15:29:59 -0400

    Add scripts to easily reorg qcow2 files and defragment

---
+++ convert_qcow2_to_uncompressed_v3_non_sparse.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ex
+
+QCOW2_OPTIONS="cluster_size=512k,lazy_refcounts=on"
+
+if [[ -z "$1" ]]; then
+	echo "Usage: $0 vm1-*.qcow2"
+	exit 0
+fi
+
+# Main loop
+for mydisk in $*
+do
+	/bin/mv -fv ${mydisk} ${mydisk}.orig
+	qemu-img convert -p -O qcow2 -o ${QCOW2_OPTIONS} ${mydisk}.orig ${mydisk} && /bin/rm -fv ${mydisk}.orig || exit 127
+done
