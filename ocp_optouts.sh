fe534a990971353fbf22cff7e9e8356cab4e8dc3
Vincent S. Cojot <vcojot@redhat.com>
2025-12-30 14:31:23 +0100

    add a few tunables

--- ocp_optouts.sh
+++ ocp_optouts.sh
@@ -10,3 +10,9 @@ rm -f .dockerconfigjson
 # OCP-V CDI
 oc patch hyperconverged kubevirt-hyperconverged -n openshift-cnv \
 --type json -p '[{"op": "replace", "path": "/spec/featureGates/enableCommonBootImageImport", "value": false}]'
+
+# Unset channel
+oc patch clusterversion version --type=json -p='[{"op": "remove", "path": "/spec/channel"}]'
+
+# Set upstream to /openshift-release.apps.ci.l2s4.p1.openshiftapps.com
+# oc patch clusterversion/version --patch '{"spec":{"upstream":"https://openshift-release.apps.ci.l2s4.p1.openshiftapps.com/graph"}}' --type=merge
