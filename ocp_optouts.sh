e5f533be85918a937b4d7dcf6daaf86067da0c72
Vincent S. Cojot <vcojot@redhat.com>
2026-01-15 21:19:30 +0100

    more disconnected changes

--- ocp_optouts.sh
+++ ocp_optouts.sh
@@ -14,5 +14,18 @@ oc patch hyperconverged kubevirt-hyperconverged -n openshift-cnv \
 # Unset channel
 oc patch clusterversion version --type=json -p='[{"op": "remove", "path": "/spec/channel"}]'
 
-# Set upstream to /openshift-release.apps.ci.l2s4.p1.openshiftapps.com
 # oc patch clusterversion/version --patch '{"spec":{"upstream":"https://openshift-release.apps.ci.l2s4.p1.openshiftapps.com/graph"}}' --type=merge
+# Set upstream to none for disconnected:
+oc patch clusterversion/version --patch '{"spec":{"upstream":"none"}}' --type=merge
+
+# Disable Telemeter client
+oc get cm -n openshift-monitoring cluster-monitoring-config -o jsonpath='{.data.config\.yaml}' > /var/tmp/cluster-monitoring-config.yaml
+printf '\n' >> /var/tmp/cluster-monitoring-config.yaml
+cat >> /var/tmp/cluster-monitoring-config.yaml <<'EOF'
+telemeterClient:
+  enabled: false
+EOF
+jq -n --arg cfg "$(cat /var/tmp/cluster-monitoring-config.yaml)" \
+  '{data:{"config.yaml":$cfg}}' > /var/tmp/cluster-monitoring-config-patch.json
+oc patch cm -n openshift-monitoring cluster-monitoring-config --type=merge \
+  --patch-file /var/tmp/cluster-monitoring-config-patch.json
